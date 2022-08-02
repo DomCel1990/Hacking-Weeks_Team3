3b46c6bfd738ad51ebc88058f63e3c1ea63a61dc
Emilio <emi00@gmail.com>
2022-08-02 18:45:28 +0200

    feat: Created Game class

---
+++ src/Game.java
@@ -0,0 +1,52 @@
+    /**
+     * @author Emilio Fagiani
+     */
+    // A class Game that implements Comparable
+    public class Game implements Comparable<Game> {
+
+        private int enemies;
+        private int score;
+
+        // Constructor
+        public Game(int enemies, int score) {
+            this.enemies = enemies;
+            this.score = score;
+        }
+
+        public int getEnemies() {
+            return enemies;
+        }
+
+        public void setEnemies(int enemies) {
+            this.enemies = enemies;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public int getFinalScore() {
+            return enemies * score;
+        }
+
+        // CompareTo methods for compare the final score
+        @Override
+        public int compareTo(Game g) {
+            int thisFinalScore = getFinalScore();
+            int otherFinalScore = g.getFinalScore();
+
+            if (thisFinalScore > otherFinalScore){
+                return 1;
+            }else if(thisFinalScore < otherFinalScore){
+                return -1;
+            }else {
+                return 0;
+            }
+        }
+
+    }
+
--- src/Start.java
+++ src/Start.java
@@ -2,4 +2,5 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Si comincia buon lavoro");
     }
+
 }
